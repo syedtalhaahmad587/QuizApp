0e8f28489a84d4e05ecf9cafd57cd60e7107cc49
Anum-Rehman <syedtalhahmed586@gmail.com>
2021-11-03 12:42:30 +0500

    timer in quiz

--- src/Component/ShowRadio.js
+++ src/Component/ShowRadio.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -33,19 +33,45 @@ const ShowRadio = (props) => {
       answer: ["XML", "SGML", "HTML 4.0", "HTML 5.0"],
       correct: "3",
     },
+    {
+      question: "HTML tag h1 tag use in close tag   :",
+      answer: ["<></>", "<//>", "<//.>", "<..>"],
+      correct: "0",
+    },
+   
 
   ]);
   //    const [ShowResult, setShowResult] = useState(false)
+  const [timer , showTimer] = useState(30);
   const [showResult , setShowResult] = useState(false)
   const [questionInd, setquestionInd] = useState(0)
   const [marks, setmarks] = useState(0);
   const quiz_attempt = () => {
     setquestionInd(questionInd + 1)
+    Timer();
 
   }
 
+  useEffect(() => {
+    Timer()
+  }, [])
+
+  const Timer = () => {
+    var time = 29;
+         setInterval(() => {
+           if(time > -1){
+            showTimer(time--)
+           }
+          else if( time = 29 ) {
+            quiz_attempt()
+          }
+          
+
+         }, 1000);
+        
+  }
+
   const changeSelection = (value) => {
-    console.log(value)
     questions[questionInd].selected = value;
     setQuestions([...questions]);
     
@@ -67,13 +93,14 @@ const ShowRadio = (props) => {
   }
   return (
     <>
-    
     {showResult ? ( 
           <Grid>     
-           <Typography variant="h3">{marks}</Typography> 
-           
+           <Typography variant="h3">{(marks / questions.length) * 100 }%</Typography> 
       </Grid>
-      ): <Grid>
+      ): <Grid className="background">
+         <Grid>
+           <Typography variant="number" className="styleNum" >{timer}</Typography>
+         </Grid>
       <Grid>
         <Typography md={12} >
           <center className="my-3" >{questions[questionInd].question}</center>
@@ -83,7 +110,7 @@ const ShowRadio = (props) => {
         <RadioGroup row aria-label="gender" name="row-radio-buttons-group">
           <Grid className="p-3" container>
             {questions[questionInd].answer.map((e, i) => (
-              <Grid md={6} key={i} >
+              <Grid  xs={6}  key={i}>
                 <Typography variant="h5" key={i}>
                   <FormControlLabel value={i} control={<Radio />} onChange={(e) => changeSelection(e.target.value)} checked={i == questions[questionInd].selected} label={e} />
                 </Typography>
@@ -95,12 +122,11 @@ const ShowRadio = (props) => {
       <Grid>
         {
           questionInd === questions.length - 1 ? <center>
-            <Button variant="contained" color="warning" onClick={Submit}>Submit</Button>
+            <Button variant="contained" color="warning" disabled={questions[questionInd].selected === undefined} onClick={Submit}>Submit</Button>
           </center> : <center>
-            <Button variant="contained" color="warning" onClick={quiz_attempt}>Next</Button>
+            <Button variant="contained" disabled={questions[questionInd].selected === undefined} color="warning" onClick={quiz_attempt}>Next</Button>
           </center>
         }
-
       </Grid>
       
     </Grid>}
